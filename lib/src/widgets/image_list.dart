77728054ae2aaa1617a6d0911eef4967b9c65af9
sholabash <sholabash106@gmail.com>
2022-12-27 17:29:43 +0100

    final

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -12,7 +12,26 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
         itemCount: images.length,
         itemBuilder: (context, int index) {
-          return Image.network(images[index].url);
+          return buildImage(images[index]);
         });
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 20.0),
+          ),
+          Text(image.title),
+        ],
+      ),
+    );
   }
 }
