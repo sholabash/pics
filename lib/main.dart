a1514296bf21b3c64f813bd2ec033b888e70147b
sholabash <sholabash106@gmail.com>
2022-12-27 03:07:17 +0100

    images

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+//I need to import a helper libary
+// from flutter to get content on the screen
+
+import 
+
+// definr a main function to run when our app starts
+
+//create a new text widget to show some text
+//on the screen
+
+//take that widget and get it on screen;
